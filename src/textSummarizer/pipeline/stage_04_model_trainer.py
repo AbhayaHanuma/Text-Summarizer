b636babb05cb62be9fe699554b940eb2ace72ffd
AbhayaHanuma <abhayabhi987@gmail.com>
2024-02-01 16:06:58 +0530

    Updated model trainer

--- src/textSummarizer/pipeline/stage_04_model_trainer.py
+++ src/textSummarizer/pipeline/stage_04_model_trainer.py
@@ -11,7 +11,7 @@ class ModelTrainerTrainingPipeline:
     def main(self):
         config = ConfigurationManager()
         model_trainer_config = config.get_model_trainer_config()
-        if os.path.join(model_trainer_config.root_dir,"pegasus-samsum-model").exits() and os.path.join(model_trainer_config.root_dir,"tokenizer").exists():
+        if os.path.exists(os.path.join(model_trainer_config.root_dir,"pegasus-samsum-model")) and os.path.exists(os.path.join(model_trainer_config.root_dir,"tokenizer")):
             logger.info(f"Trained model already exists so skipping the training")
         else:
             model_trainer_config = ModelTrainer(config=model_trainer_config)
